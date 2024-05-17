46b5f5f5d60d0a918f100a96d244d9377daa6a26
owenliu01 <59181616+owenliu01@users.noreply.github.com>
2024-05-17 22:28:09 +0000

    Change scrolling behaviour

--- components/loader.js
+++ components/loader.js
@@ -1,6 +1,9 @@
 let loading = document.getElementById("loading");
 setTimeout(function(){
     loading.style.opacity = 0;
+    loading.addEventListener('wheel', function(e) {
+        e.preventDefault();
+    }, { passive: false });
     setTimeout(function(){
         loading.style.display = "none"
     },1000);
