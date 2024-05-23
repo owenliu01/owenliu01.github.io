0ad26d8a700b58586b87d0983732a0063b000817
owenliu01 <59181616+owenliu01@users.noreply.github.com>
2024-05-23 20:42:56 +0000

    alter scrolling script

--- components/loader.js
+++ components/loader.js
@@ -1,10 +1,10 @@
 let loading = document.getElementById("loading");
 setTimeout(function(){
     loading.style.opacity = 0;
-    window.addEventListener('wheel', preventScroll, {passive: false});
+    window.removeEventListener('wheel', preventScroll, {passive: false});
     setTimeout(function(){
         loading.style.display = "none"
-        window.removeEventListener('wheel',preventScroll, {passive: fasle});
+        window.addEventListener('wheel',preventScroll, {passive: fasle});
     },1000);
 },3000);
 
