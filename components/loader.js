167af67cd265c3262b20e60c89e54f711539218a
owenliu01 <59181616+owenliu01@users.noreply.github.com>
2024-05-23 21:31:45 +0000

    Fix loading timing

--- components/loader.js
+++ components/loader.js
@@ -7,7 +7,7 @@ setTimeout(function(){
     setTimeout(function(){
         loading.style.display = "none";
         window.removeEventListener('wheel', preventScroll, {passive: false});
-    },1000);
+    },990);
 },3000);
 
 function preventScroll(e){
