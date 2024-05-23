86ee24cded0dcbf32572ecc1a7723f19b371be35
owenliu01 <59181616+owenliu01@users.noreply.github.com>
2024-05-23 20:39:33 +0000

    Change no scrolling script

--- components/loader.js
+++ components/loader.js
@@ -1,11 +1,13 @@
 let loading = document.getElementById("loading");
 setTimeout(function(){
     loading.style.opacity = 0;
-    var yourDiv = document.getElementById("loading");
-    yourDiv.addEventListener('wheel', function(e) {
-        e.preventDefault();
-    }, { passive: false });
+    window.addEventListener('wheel', preventScroll, {passive: false});
     setTimeout(function(){
         loading.style.display = "none"
+        window.removeEventListener('wheel',preventScroll, {passive: fasle});
     },1000);
 },3000);
+
+function preventScroll(e){
+    e.preventDefault();
+}
